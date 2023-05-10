4e82ef4af323ca94f2c89ed48197f77e4cdc4972
Jürgen Ryannel <juergen@ryannel.org>
2023-05-10 12:57:07 +0200

    expose core types from init

---
+++ src/olink/core/__init__.py
@@ -0,0 +1,8 @@
+from .types import Name as Name
+from .types import LogLevel as LogLevel
+from .types import MsgType as MsgType
+from .types import Base as Base
+from .types import ILogger as ILogger
+from .node import BaseNode as BaseNode
+from .protocol import IProtocolListener as IProtocolListener
+from .protocol import Protocol as Protocol
