568baa4bf3315ace7d6a4006009f1e6187dcd82a
Wolfgang Bremer <wbremer@apigear.io>
2026-01-14 19:59:28 +0100

    fix: ensure independent state for ClientNode instances

--- tests/test_clientnode.py
+++ tests/test_clientnode.py
@@ -35,3 +35,27 @@ def test_detach_node_from_all_sinks():
     assert r.get_node(name) == client
     client.detach()
     assert r.get_node(name) == None
+
+
+def test_client_node_invokes_pending_isolation():
+    """Two ClientNode instances should have independent invokes_pending dicts"""
+    node1 = ClientNode()
+    node2 = ClientNode()
+
+    node1.invokes_pending[1] = lambda x: None
+
+    assert 1 not in node2.invokes_pending, "node2 should not see node1's pending invokes"
+    assert len(node2.invokes_pending) == 0, "node2 should have empty invokes_pending"
+
+
+def test_client_node_request_id_isolation():
+    """Two ClientNode instances should have independent requestId counters"""
+    node1 = ClientNode()
+    node2 = ClientNode()
+
+    _ = node1.next_request_id()
+    _ = node1.next_request_id()
+    _ = node1.next_request_id()
+
+    id2 = node2.next_request_id()
+    assert id2 == 1, f"node2 should start at 1, got {id2}"
